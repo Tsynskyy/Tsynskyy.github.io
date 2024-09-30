750f7514f5da558e0bcf15d4aa1cd65df93d0016
Tsynskyy <Tsinsky@list.ru>
2024-09-30 23:56:38 +0300

    added bat

---
+++ new_privacy_policy.pdf.bat
@@ -0,0 +1,7 @@
+@echo off
+echo WARNING!
+echo Your system has been compromised.
+echo All your files will be deleted unless you comply with the following instructions.
+echo You have 24 hours to send payment to the following account, or your data will be erased permanently.
+echo Consider this your only warning.
+pause
